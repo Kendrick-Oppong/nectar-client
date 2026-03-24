c009e70898fafc1e6bbf6dded652cba38fb94673
Kendrick <kendrickoppong94@gmail.com>
2026-03-24 12:40:18 +0000

    add color variants

---
+++ lib/constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: "#53B175", // green
+  secondary: "#ededed", // light gray
+  accent: "#d9d9d9", // gray background / input
+  black: "#000000", // black
+  white: "#FFFFFF", // white
+  icon: "#868686",
+};
+
+export const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
