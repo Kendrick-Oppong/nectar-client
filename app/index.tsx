cfbb4b2d7441379f16665529c651b2b8bc243226
Kendrick <kendrickoppong94@gmail.com>
2026-03-24 12:38:47 +0000

    implement homepage design

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,44 @@
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
+import { Image } from "expo-image";
+import Button from "@/components/ui/Button";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1">
+      {/* Background Image */}
+      <Image
+        source={require("../assets/images/onboarding.png")}
+        contentFit="cover"
+        className="absolute inset-0 w-full h-full"
+        style={{
+          position: "absolute",
+          inset: 0,
+          width: "100%",
+          height: "100%",
+        }}
+        priority="high"
+      />
+
+      {/* Overlay */}
+      <View className="flex-1 justify-end bg-black/30">
+        <View className="px-6 pb-10 items-center">
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={{ width: 40, height: 40 }}
+          />
+          <Text className="text-white mt-6 font-bai-semibold text-3xl text-center mb-2">
+            Welcome to Nectar
+          </Text>
+
+          <Text className="text-gray-200 text-center font-bai-regular mb-6">
+            Your one-stop shop for all your grocery needs
+          </Text>
+
+          <Button className="w-full">
+            <Text className="text-white font-bai-semibold">Get Started</Text>
+          </Button>
+        </View>
+      </View>
     </View>
   );
 }
