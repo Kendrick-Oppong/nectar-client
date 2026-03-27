7f431911aef58f8c7f973901850945b05027ee48
Kendrick <kendrickoppong94@gmail.com>
2026-03-27 22:17:36 +0000

    add api endpoints

---
+++ lib/constants/api-endpoints.ts
@@ -0,0 +1,15 @@
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL!;
+
+export const API_ENDPOINTS = {
+  AUTH: {
+    LOGIN: "/auth/login",
+    REGISTER: "/auth/register",
+    FORGOT_PASSWORD: "/auth/forgot-password",
+    RESET_PASSWORD: "/auth/reset-password",
+    VERIFY_EMAIL: "/auth/verify-email",
+    RESEND_VERIFICATION_EMAIL: "/auth/resend-verification-email",
+    CHANGE_PASSWORD: "/auth/change-password",
+    LOGOUT: "/auth/logout",
+    REFRESH_TOKEN: "/auth/refresh",
+  },
+};
