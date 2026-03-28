0d7a4566cecaf78958cd79133554ce142e323614
Kendrick <kendrickoppong94@gmail.com>
2026-03-28 09:57:51 +0000

    extend props

--- components/ui/ScreenContainer.tsx
+++ components/ui/ScreenContainer.tsx
@@ -1,11 +1,14 @@
 import { cn } from "@/lib/utils/classNames";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  SafeAreaView,
+  SafeAreaViewProps,
+} from "react-native-safe-area-context";
 import { withUniwind } from "uniwind";
 
 const StyledSafeAreaView = withUniwind(SafeAreaView);
 
-interface ScreenContainerProps {
+interface ScreenContainerProps extends SafeAreaViewProps {
   children: React.ReactNode;
   className?: string;
 }
@@ -13,9 +16,13 @@ interface ScreenContainerProps {
 export default function ScreenContainer({
   children,
   className,
+  ...props
 }: Readonly<ScreenContainerProps>) {
   return (
-    <StyledSafeAreaView className={cn("flex-1 bg-white p-4", className)}>
+    <StyledSafeAreaView
+      {...props}
+      className={cn("flex-1 bg-white p-4", className)}
+    >
       {children}
     </StyledSafeAreaView>
   );
