b39bc5937561243a76160ea9e4555a6cbffdca3f
Kendrick <kendrickoppong94@gmail.com>
2026-03-29 23:21:43 +0000

    remove file

--- app/(auth)/onboarding.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function OnboardingScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Onboarding</Text>
-    </View>
-  );
-}
